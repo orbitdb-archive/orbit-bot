122cfe42b8af8983befaa34ad801cc26b0e0cbfc
haad <haad@headbanggames.com>
2017-03-23 16:05:59 +0100

    Update to use latest orbit and ipfs modules

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 
 'use strict'
 
-const IpfsDaemon = require('ipfs-daemon')
+const IpfsDaemon = require('ipfs-daemon/src/ipfs-node-daemon')
 const Orbit = require('orbit_')
 
 // Options
@@ -35,30 +35,41 @@ function formatTimestamp(timestamp) {
 // Start an IPFS daemon
 const ipfs = new IpfsDaemon({ 
   IpfsDataDir: ipfsDataDir,
+  SignalServer: 'star-signal.cloud.ipfs.team', // IPFS dev server
   Addresses: {
     API: '/ip4/127.0.0.1/tcp/0',
     Swarm: [
       '/ip4/0.0.0.0/tcp/0'
     ],
     Gateway: '/ip4/0.0.0.0/tcp/0'
-  }
+  },
+  Discovery: {
+    MDNS: {
+      Enabled: false,
+      Interval: 10
+    },
+    webRTCStar: {
+      Enabled: true
+    }
+  },
 })
 
 ipfs.on('ready', () => {
   // Output the IPFS id
-  ipfs.id().then((id) => {
-    console.log("Peer ID:")
-    console.log(">", id.id)
-    console.log("Addresses:")
-    id.addresses.forEach((e) => console.log(">", e))
-    console.log("Public Key:")
-    console.log(">", id.publicKey)
-  })
+  ipfs.id()
+    .then((id) => {
+      console.log("Peer ID:")
+      console.log(">", id.id)
+      console.log("Addresses:")
+      id.addresses.forEach((e) => console.log(">", e))
+      console.log("Public Key:")
+      console.log(">", id.publicKey)
+    })
 
   // Setup Orbit
   const options = {
     cachePath: dataDir + '/orbit-db',
-    maxHistory: 0, 
+    maxHistory: 10,
     keystorePath: dataDir + '/keystore'
   }
 
@@ -73,9 +84,21 @@ ipfs.on('ready', () => {
       console.log(`-!- Joined #${channel}`)    
       let pingCount = 1
 
+      const feed = orbit.channels[channel].feed
+
       // Handle new messages
       orbit.events.on('message', (channel, post) => {
         console.log(`${formatTimestamp(post.meta.ts)} < ${post.meta.from.name}> ${post.content}`)
+      })
+
+      feed.events.on('synced', (channel) => {
+        orbit.get(channel, null, null, 10)
+          .then((messages) => {
+            messages.forEach(message => {
+              console.log(`${formatTimestamp(message.Post.meta.ts)} < ${message.Post.meta.from.name}> ${message.Post.content}`)
+            })
+          })
+          .catch((e) => console.error(e))
       })
 
       // Send message at intervals
